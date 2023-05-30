f9e90fcace55481169f4ce6fe5babe48d91265a0
kushanMadusha96 <kushanmadusha96@gmail.com>
2023-05-31 02:11:22 +0530

    add loose-cupling and DI to customer section

---
+++ src/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import model.CustomerDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CustomerDAO {
+     ArrayList<CustomerDTO> loadAllCustomer() throws SQLException, ClassNotFoundException;
+     void saveCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException;
+     void updateCustomer(String id, String name, String address) throws SQLException, ClassNotFoundException;
+     boolean isExistCustomer(String id) throws SQLException, ClassNotFoundException;
+     void deleteCustomer(String id) throws SQLException, ClassNotFoundException;
+     String genarateNewId() throws SQLException, ClassNotFoundException;
+     CustomerDTO searchCustomer(String newValue) throws SQLException, ClassNotFoundException;
+}
